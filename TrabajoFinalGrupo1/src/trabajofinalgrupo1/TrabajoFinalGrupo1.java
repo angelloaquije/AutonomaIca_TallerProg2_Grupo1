00a62be11af908872784c702afd472b90bbf1147
angelloaquije <angelloaquije@gmail.com>
2021-11-06 10:00:33 -0500

    c

--- TrabajoFinalGrupo1/src/trabajofinalgrupo1/TrabajoFinalGrupo1.java
+++ TrabajoFinalGrupo1/src/trabajofinalgrupo1/TrabajoFinalGrupo1.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package trabajofinalgrupo1;
 
 /**
@@ -11,11 +7,9 @@ package trabajofinalgrupo1;
  */
 public class TrabajoFinalGrupo1 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
     
 }
